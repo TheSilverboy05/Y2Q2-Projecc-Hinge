b69240c8710c25f91e79ffc0200c865341149c15
TheSilverboy05 <silvanbeek@ziggo.nl>
2024-11-18 16:54:15 +0100

    Added constants file

--- Master file.py
+++ Master file.py
@@ -1,2 +1,22 @@
 #This is the master file 
 #Please do not edit this unless you're doing something useful
+
+#Import files
+import Constants #constants
+    #input configuration
+
+#Input parameters
+    #put those here
+
+#Functions
+    #define those here
+
+#Main code loop
+    #do math or smth
+
+#Testing loop
+print(Constants.g0)
+
+
+#Outputs
+    #Yippee
