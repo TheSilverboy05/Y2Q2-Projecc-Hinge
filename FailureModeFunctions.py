7c9bd3347b346a6ec100f607a6a4cab35ac28ad1
Seppe Boone <108073472+seppeboone44@users.noreply.github.com>
2024-11-20 16:05:06 +0100

    changes

---
+++ FailureModeFunctions.py
@@ -0,0 +1,8 @@
+
+
+
+def PullThrough(a,b,c):
+
+    SF = "some calculation"
+    return SF
+
--- Master file.py
+++ Master file.py
@@ -1,12 +1,18 @@
 #This is the master file 
 #Please do not edit this unless you're doing something useful
+import numpy as np
+import math
+import matplotlip.pyplot as plt
+import FailureModeFunctions as FM
 
 #Import files
-import constants #constants
-    #input configuration
-        #Dimensions of flanges
-        #Fastener pattern
-        #Fastener type
+import constants as cst
+import Configuration_I as config
+
+
+    #Dimensions of flanges
+    #Fastener pattern
+    #Fastener type
 
 
 #Input parameters
@@ -22,7 +28,7 @@ import constants #constants
     #do math or smth
 
 #Testing loop
-print(constants.g0)
+print(cst.g0)
 
 
 #Outputs
