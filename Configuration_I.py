a4fb024b7fb9082cb0d1fd3bdf3f37b2efe77a3d
Seppe Boone <108073472+seppeboone44@users.noreply.github.com>
2024-11-20 15:30:39 +0100

    Configuration File

---
+++ Configuration_I.py
@@ -0,0 +1,23 @@
+# Configuration template
+
+# Spacecraft skin:
+t3      = 2     * 10**(-3)      #[m]        Thickness
+Eskin   = 72    * 10**(9)       #[Pa]       Young Modulus
+Gskin   = 27    * 10**(9)       #[Pa]       Shear Modulus
+
+# Back Plate:
+t2      = 5     * 10**(-3)      #[m]        Thickness
+L       = 250   * 10**(-3)      #[m]        Lenght of the back plate
+w       = 100   * 10**(-3)      #[m]        Height of the back plate
+D2      = 4     * 10**(-3)      #[m]        Diameter of bolts/rivets
+n       = 4                     #           Number of bolts in the back plate (total)
+e1      = 5     * 10**(-3)      #[m]        distance edge to center of a hole on the y-axis (along w dimension)
+e2      = 5     * 10**(-3)      #[m]        distance edge to center of a hole on the x-axis (along L dimension)
+
+# Flange
+s1      = 100   * 10**(-3)      #[m]        Length of the flange
+s2      = 70    * 10**(-3)      #[m]        Center position of the pin hole
+D1      = 30    * 10**(-3)      #[m]        Diameter of the pin hole
+h       = 50    * 10**(-3)      #[m]        Inner dimension between two flanges
+N       = 2                     #           Number of flanges
+
