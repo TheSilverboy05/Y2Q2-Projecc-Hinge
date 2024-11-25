4705ffbae68204739d094e835b09d1a00aff9183
Seppe Boone <seppeboone44@gmail.com>
2024-11-25 15:17:06 +0100

    Zo komen we der

--- FailureModeFunctions.py
+++ FailureModeFunctions.py
@@ -1,3 +1,30 @@
+def Forces(Fx, Fy, Fz, H, W):
+    """
+    Returns a list [Ax, Ay, Az, Bx, By, Bz, M_Ay,M_Ay, M_Az, M_Bz]
+    Fx = Force in x-direction
+    Fy = Force in y-direction
+    Fz = Force in z-direction
+    H  = Height of the solar panel in z-direction
+    W  = length of the solar panel
+    """
+    #Forces
+    Az = -Fz/2
+    Bz = -Fz/2
+    Ax = -Fx/2
+    Bx = -Fx/2
+    Ay = (Fz*W-Fy*H)/(2*H)
+    By = -Fy-Ay
+
+
+    # Moments
+    M_Ay = -(Fx * H)/8
+    M_By = (Fx * H)/8
+    M_Az = (Fx * W)/4
+    M_Bz = (Fx * W)/4
+
+    return [Ax, Ay, Az, Bx, By, Bz, M_Ay,M_Ay, M_Az, M_Bz]
+
+
 def FlangeFailure(W,D,t,F_ty,F_y,F_z):
 
     #These need to be redone
@@ -21,4 +48,5 @@ def FlangeFailure(W,D,t,F_ty,F_y,F_z):
 
     SF = (1/((R_a^1.6+R_tr^1.6)^0.625))-1
     return SF
+
 
