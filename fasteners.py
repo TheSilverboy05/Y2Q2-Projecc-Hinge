a2a0610db05a0781e5cf3989c574ce1682043e1c
theo0o47 <77543981+theo0o47@users.noreply.github.com>
2024-11-25 15:28:42 +0100

    da

--- fasteners.py
+++ fasteners.py
@@ -1,16 +1,12 @@
-
-n = 4 
-Fx=5
-Fy=5
-D_2=1
-t_2=1
-
+import Configuration_I as c
+Fx=c.Fx/2
+Fy=c.Fy/2
 F_xbolt=Fx/n
 F_ybolt=Fy/n
 
 P = (F_xbolt**2+F_ybolt**2)**0.5
 
 sigma=P/(D_2*t_2)
-
+#then compare sigma to the one of the material max strenght and see 
 
 print(sigma)
