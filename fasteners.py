b503178439e68d70ed09b5441059bd1a43aa6a76
theo0o47 <77543981+theo0o47@users.noreply.github.com>
2024-11-25 16:14:13 +0100

    da

--- fasteners.py
+++ fasteners.py
@@ -1,12 +1,22 @@
 import Configuration_I as c
-Fx=c.Fx/2
-Fy=c.Fy/2
-F_xbolt=Fx/n
-F_ybolt=Fy/n
+Fx=c.Fx
+Fz=c.Fz
+My=c.My
 
-P = (F_xbolt**2+F_ybolt**2)**0.5
 
-sigma=P/(D_2*t_2)
+x=6   #this depends on the number of fasteners used and their positions
+z=1
+
+
+
+F_xbolt=Fx/n + My/(n*x)
+F_zbolt=Fz/n + My/(n*z)
+
+
+
+P = (F_xbolt**2+F_zbolt**2)**0.5
+
+sigma=P/(D_2*t_2)    #max stress experienced by the bolt
 #then compare sigma to the one of the material max strenght and see 
 
 print(sigma)
