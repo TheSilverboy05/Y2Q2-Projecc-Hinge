eb759d35841e53654681ca691ed857289a7a3c7c
TheSilverboy05 <silvanbeek@ziggo.nl>
2024-12-04 17:34:39 +0100

    yea whatever

--- SilTesting.py
+++ SilTesting.py
@@ -19,32 +19,35 @@ Forces = FM.Forces(config.Fx, config.Fy, config.Fz, config.H, config.W)
 
 # Sil testing
 SF = FM.FlangeFailure(config.w, config.D1, config.t1, 70000000, Forces[2], Forces[3])
-D_range = 1 * 10**(-3)
-W_range = 1 * 10**(-3)
-t_range = 0.5 * 10**(-3)
+D_range = 10 * 10**(-3)
+W_range = 10 * 10**(-3)
+t_range = 1 * 10**(-3)
 
-D_step = 0.1 * 10**(-3)
-W_step = 0.1 * 10**(-3)
-t_step = 0.05 * 10**(-3)
+D_step = 1 * 10**(-3)
+W_step = 1 * 10**(-3)
+t_step = 0.1 * 10**(-3)
 
 LVF = 100
 
 Dx = config.D1-D_range
 while Dx < (config.D1+D_range):
     Wx = config.w-W_range
-    while Wx < (config.w+W_range):
-        tx = config.t1-t_range
-        while tx < (config.w+W_range):
-            SF = FM.FlangeFailure(Wx, Dx, tx, 70000000, Forces[2], Forces[3])
-            VF  = config.s2*Wx*tx + 0.5*math.pi*((0.5*Wx)**2)*tx - math.pi*((Dx/2)**2)*tx
-            print(Dx, Wx, tx, SF)
-            if VF < LVF and SF > 1.25:
-                LVF = VF
-                Config = [Dx, Wx, tx]
-            tx = tx + t_step
+    if Wx <= Dx:
         Wx = Wx + W_step
+    else:
+        while Wx < (config.w+W_range):
+            tx = config.t1-t_range
+            while tx < (config.w+W_range):
+                SF = FM.FlangeFailure(Wx, Dx, tx, 510000, Forces[2], Forces[3])
+                VF  = config.s2*Wx*tx + 0.5*math.pi*((0.5*Wx)**2)*tx - math.pi*((Dx/2)**2)*tx
+                print(Dx, Wx, tx, SF)
+                if VF < LVF and SF > 1.25:
+                    LVF = VF
+                    Config = [Dx, Wx, tx]
+                tx = tx + t_step
+            Wx = Wx + W_step
     Dx = Dx + D_step
 print(Config)
 
-print("SF: ", FM.FlangeFailure(Config[1], Config[0], Config[2], 70000000, Forces[2], Forces[3]), "VF: ", LVF)
+print("SF: ", FM.FlangeFailure(Config[1], Config[0], Config[2], 510000, Forces[2], Forces[3]), "VF: ", LVF)
 
